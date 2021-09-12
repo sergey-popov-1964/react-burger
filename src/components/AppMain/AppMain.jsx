252392049ced9c8338069411f3fdf50beb9eca35
Sergey Popov <sergeykms@gmail.com>
2021-09-12 20:45:13 +1000

    12_09_2021__2

---
+++ src/components/AppMain/AppMain.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import style from './AppMain.module.css'
+
+// import {Logo, BurgerIcon, ListIcon, ProfileIcon} from "@ya.praktikum/react-developer-burger-ui-components";
+
+function AppMain() {
+  return (
+    <div className={style.block}>
+      {/*<BurgerIngredients/>*/}
+      {/*<BurgerConstructor/>*/}
+    </div>
+  );
+}
+
+export default AppMain;
