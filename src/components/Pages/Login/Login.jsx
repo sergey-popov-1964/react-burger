53a6e4ffb8edfef9d5c3d1aa4d0e530599d972fd
Sergey Popov <sergeykms@gmail.com>
2021-10-07 22:22:20 +1000

    07_10_2021__2
    Login

--- src/components/Pages/Login/Login.jsx
+++ src/components/Pages/Login/Login.jsx
@@ -1,12 +1,105 @@
-import React from 'react';
+import React, {useState} from 'react';
 import '../../../index.css'
 import styles from './Login.module.css'
+import {Button, Input} from "@ya.praktikum/react-developer-burger-ui-components";
+import {Link} from "react-router-dom";
+import style from "../Main/BurgerIngredients/BurgerIngredients.module.css";
 
 function Login(props) {
+
+  // const [currentError, setCurrentError] = useState("");
+  const [loginState, setLoginState] = useState(
+    {
+      email: '',
+      password: '',
+    }
+  )
+  // const [isValid, setIsValid] = useState(false);
+  // const [errorMessageEmail, setErrorMessageEmail] = useState("l")
+  // const [errorMessagePassword, setErrorMessagePassword] = useState("")
+
+  // useEffect(() => {
+  //   const emailValidity = loginState.email.match(/^[\w-\.\d*]+@[\w\d]+(\.\w{2,4})$/);
+  //   const passwordValidity = loginState.password.match(/(?=.*[0-9])(?=.*[!@#$%^&*])(?=.*[a-z])(?=.*[A-Z])[0-9a-zA-Z!@#$%^&*]{6,}/g);
+  //   emailValidity ? setErrorMessageEmail("") : setErrorMessageEmail("Поле должно содержать e-mail")
+  //   passwordValidity ? setErrorMessagePassword("") : setErrorMessagePassword("Пароль должен быть длиной не менее 6 символов и содержать спецсимвол, цифру, латинскую букву в верхнем и нижнем регистре")
+  //   setIsValid(emailValidity && passwordValidity);
+  // }, [loginState.email, loginState.password])
+  //
+  // function typeError(data) {
+  //   setCurrentError(data)
+  // }
+
+  function handleChange(e) {
+    console.log(e.target.value)
+    const {name, value} = e.target;
+    setLoginState(prevState => ({...prevState, [name]: value}));
+  }
+
+  function handleSubmit(e) {
+    e.preventDefault();
+    console.log(1111111111)
+    // onLogin(loginState, typeError)
+  }
+
+  function onIconClick() {
+
+  }
+
+  const inputRef = React.useRef(null)
+
+
   return (
     <div className="block">
-      <h1 className={styles.title}>Login</h1>
+      <form action="#"
+            onSubmit={handleSubmit}
+            className={styles.login__form}
+            name='login' noValidate>
+        <h2 className={styles.login__title}>Вход</h2>
+        <div className={styles.login__input}>
+          <Input
+            type={'email'}
+            placeholder={'e-mail'}
+            onChange={handleChange}
+            value={loginState.email}
+            name={'email'}
+            error={false}
+            ref={inputRef}
+            errorText={'Ошибка'}
+            size={'default'}
+          />
+        </div>
+        <div className={styles.login__input}>
+          <Input
+            type={'password'}
+            placeholder={'пароль'}
+            onChange={handleChange}
+            icon={'ShowIcon'}
+            value={loginState.password}
+            name={'password'}
+            error={false}
+            ref={inputRef}
+            onIconClick={onIconClick}
+            errorText={'Ошибка'}
+            size={'default'}
+          />
+        </div>
+
+        <div className={styles.login__button}>
+          <Button type="primary" size="small">
+            Войти
+          </Button>
+        </div>
+        <p className={`${styles.login__text} text text_type_main-small`}>Вы - новый пользователь?&nbsp;
+          <Link className={styles.login__link} to='/register'>Регистрация</Link>
+        </p>
+        <p className={`${styles.login__text} text text_type_main-small`}>Забыли пароль?&nbsp;
+          <Link className={styles.login__link} to='/forgot-password'>Восстановить пароль</Link>
+        </p>
+      </form>
     </div>
+
+
   );
 }
 
